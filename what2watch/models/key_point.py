0039585eb7d1b42147abbc3f5427425e35681170
krisztianszenasi <szenasi999@gmail.com>
2024-05-12 19:06:56 +0200

    ✨ feat(models): Upgrade `KeyPoint` model with `TranscriptChunk` relation.

--- what2watch/models/key_point.py
+++ what2watch/models/key_point.py
@@ -6,5 +6,6 @@ class KeyPoint(db.Model):
     _id = db.Column('id', db.Integer, primary_key=True)
     video_id = db.Column(db.String(14), db.ForeignKey('video.id'))
     text = db.Column(db.Text)
-    timestamp = db.Column(db.Float)
     video = db.relationship('Video', back_populates='key_points')
+    starts_from_id = db.Column(db.Integer, db.ForeignKey('transcript_chunk.id'), nullable=True)
+    starts_from = db.relationship('TranscriptChunk')
