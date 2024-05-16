258d0cdd42bc457030ba41994413789d8d070821
krisztianszenasi <szenasi999@gmail.com>
2024-05-16 21:30:26 +0200

    🔨 task(create_app): Handle missing OpenAI api key.

--- what2watch/__init__.py
+++ what2watch/__init__.py
@@ -3,10 +3,16 @@ from flask import Flask
 from what2watch.extensions import db, migrate
 from dotenv import load_dotenv
 from what2watch.routes.video import video
+import os
 
 
 def create_app(config='what2watch.config.default'):
     load_dotenv()
+
+    if os.environ.get('OPENAI_API_KEY') is None:
+        print('\nPlease set your OpenAI API key as an environmental variable: OPENAI_API_KEY')
+        print('\nGet one here: https://platform.openai.com/')
+        exit()
 
     app = Flask(__name__)
     app.config.from_object(config)
